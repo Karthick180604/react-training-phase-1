6f00b95d7d2ec48ca90c2908935d2bf6bb94db11
Karthick180604 <karthick.v@rently.com>
2025-06-16 17:33:06 +0530

    practiced closures, spread and rest operators, arrow functions..

---
+++ practice.js
@@ -0,0 +1,50 @@
+console.log(x)
+var x=10;
+
+
+function myCounter(){
+    let count=0;
+    return function(){
+        count++;
+        return count;
+    }
+}
+const add = myCounter()
+console.log(add())
+console.log(add());
+console.log(add());
+
+const obj={
+    name:"john",
+    getName:function(){
+        return this.name;
+    },
+    getNestedName:function(){
+        return function(){
+            return this.name;
+        }
+    },
+    getThroughArrow:()=>{
+        return this.name;
+    },
+    getNestedArrow:function(){
+        return () => {
+            return this.name;
+        }
+    }
+}
+console.log(obj.getName())
+const callFunction=obj.getNestedName();
+console.log(callFunction())
+console.log("Arrow",obj.getThroughArrow())
+const callArrowFunction=obj.getNestedArrow();
+console.log("nested arrow", callArrowFunction())
+
+const array=[1,2,3,4,5]
+const[one, two, ...rest]=array
+console.log(one, two, rest)
+
+function restOperator(...num){
+    console.log(num)
+}
+restOperator(1,2,3,4,5)
